4da76d14bc011ec0f029d44d3d1d046e829225f6
wuyeelok <wuyeelok@gmail.com>
2025-08-22 22:24:20 +0800

    Use cowsay module

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cowsay as cs
 
 print("This is first Python program!")
 
@@ -8,5 +9,8 @@ s=np.pi/6
 a=np.sin(c)+np.cos(s)
 
 print(a)
+
+
+cs.cow("Hello, this is a cow saying hello!")
 
 input("Press Enter to continue...")
